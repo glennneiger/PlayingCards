c1fe947e2ab34ae9870621a0f7e5f69c343ed4f5
Hugo Ángeles Chávez <hugoangeles0810@gmail.com>
2018-04-08 21:16:42 -0500

    Animating cards

---
+++ Playing Cards/PlayingCardBehavior.swift
@@ -0,0 +1,73 @@
+//
+//  PlayingCardBehavior.swift
+//  Playing Cards
+//
+//  Created by Hugo Ángeles Chávez on 4/8/18.
+//  Copyright © 2018 Hugo Ángeles Chávez. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardBehavior: UIDynamicBehavior {
+    
+    
+    lazy var collisionBehavior: UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+       let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = false
+        behavior.elasticity = 1
+        behavior.resistance = 0
+        return behavior
+    }()
+    
+    func addItem(_ item: UIDynamicItem) {
+        collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
+        push(item)
+    }
+    
+    func removeItem(_ item: UIDynamicItem) {
+        collisionBehavior.removeItem(item)
+        itemBehavior.removeItem(item)
+    }
+    
+    private func push(_ item: UIDynamicItem) {
+        let push = UIPushBehavior(items: [item], mode: .instantaneous)
+        if let referenceBounds = dynamicAnimator?.referenceView?.bounds {
+            let center = CGPoint(x: referenceBounds.midX, y: referenceBounds.midY)
+            switch (item.center.x, item.center.y) {
+            case let (x, y) where x < center.x && y < center.y:
+                push.angle = (CGFloat.pi/2).arc4random
+            case let (x, y) where x > center.x && y < center.y:
+                push.angle = CGFloat.pi-(CGFloat.pi/2).arc4random
+            case let (x, y) where x < center.x && y > center.y:
+                push.angle = (-CGFloat.pi/2).arc4random
+            case let (x, y) where x > center.x && y > center.y:
+                push.angle = CGFloat.pi+(CGFloat.pi/2).arc4random
+            default:
+                push.angle = (CGFloat.pi*2).arc4random
+            }
+        }
+        push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
+        push.action = { [unowned push, weak self] in
+            self?.removeChildBehavior(push)
+        }
+        addChildBehavior(push)
+    }
+    
+    override init() {
+        super.init()
+        addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
+    }
+    
+    convenience init(in animator: UIDynamicAnimator) {
+        self.init()
+        animator.addBehavior(self)
+    }
+}
--- Playing Cards/ViewController.swift
+++ Playing Cards/ViewController.swift
@@ -9,17 +9,117 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var cardViews: [PlayingCardView]!
+    private var deck = PlayingCardDeck()
+    
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    
+    lazy var cardBehavior = PlayingCardBehavior(in: animator)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var cards = [PlayingCard]()
+        
+        for _ in 1...((cardViews.count + 1) / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        
+        for cardView in cardViews {
+            cardView.isFaceUp = false
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+            cardBehavior.addItem(cardView)
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var cardsMatch: Bool {
+        return faceUpCardViews.count == 2 &&
+                faceUpCardViews[0].rank == faceUpCardViews[1].rank &&
+                faceUpCardViews[0].suit == faceUpCardViews[1].suit
     }
-
-
+    
+    private var faceUpCardViews: [PlayingCardView] {
+        return cardViews.filter { $0.isFaceUp && !$0.isHidden }
+    }
+    
+    @objc
+    func flipCard(_ recognizer: UITapGestureRecognizer) {
+        switch recognizer.state {
+        case .ended:
+            if let choosenCardView = recognizer.view as? PlayingCardView {
+                handleCardViewTap(choosenCardView)
+            }
+        default:
+            break
+        }
+    }
+    
+    fileprivate func handleCardViewTap(_ choosenCardView: PlayingCardView) {
+        if faceUpCardViews.count >= 2 {
+            return
+        }
+        
+        cardBehavior.removeItem(choosenCardView)
+        UIView.transition(with: choosenCardView,
+              duration: 0.5,
+              options: [.transitionFlipFromLeft],
+              animations: {
+                choosenCardView.isFaceUp = !choosenCardView.isFaceUp
+              },
+              completion: { finished in
+                let faceUpCards = self.faceUpCardViews
+                if self.cardsMatch {
+                    faceUpCards.forEach { cardView in
+                        self.animateMatchedCards(cardView)
+                    }
+                } else if faceUpCards.count == 2 {
+                    self.faceDownCards(faceUpCards)
+                }
+            })
+    }
+    
+    fileprivate func faceDownCards(_ faceUpCards: [PlayingCardView]) {
+        faceUpCards.forEach { cardView in
+            UIView.transition(with: cardView,
+                              duration: 0.5,
+                              options: [.transitionFlipFromLeft],
+                              animations: {
+                                cardView.isFaceUp = false
+                              },
+                              completion: { finished in
+                                    self.cardBehavior.addItem(cardView)
+                              })
+        }
+    }
+    
+    fileprivate func animateMatchedCards(_ cardView: PlayingCardView) {
+        UIViewPropertyAnimator.runningPropertyAnimator(
+            withDuration: 1.0,
+            delay: 0,
+            options: [],
+            animations: {
+                cardView.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
+        },
+            completion: { finished in
+                UIViewPropertyAnimator.runningPropertyAnimator(
+                    withDuration: 1.0,
+                    delay: 0,
+                    options: [],
+                    animations: {
+                        cardView.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
+                        cardView.alpha = 0
+                },
+                    completion: { finished in
+                        cardView.isHidden = true
+                        cardView.alpha = 1
+                        cardView.transform = CGAffineTransform.identity
+                })
+        })
+    }
 }
 
